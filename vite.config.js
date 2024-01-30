73cf9c60781f79d991e42a592163647e73de7754
Okocha Emeka Emmanuel <emekaokocha07@gmail.com>
2024-01-30 14:23:50 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import reactIcons from 'react-icons/fa6';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [reactIcons],
+    },
+  },
 })
