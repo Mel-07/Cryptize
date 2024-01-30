3be236aa2751b84344a9fef8f0fb7471f116e968
Okocha Emeka Emmanuel <emekaokocha07@gmail.com>
2024-01-30 15:58:08 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons/fa','react-icons/fa6', 'react-icons/fi', 'react-icons/md', 'react-icons/io', 'react-icons/go', 'react-icons/ti', 'react-icons/ai', 'react-icons/gr', 'react-icons/im', 'react-icons/bi', 'react-icons/bs', 'react-icons/cg', 'react-icons/fc', 'react-icons/hi', 'react-icons/io5', 'react-icons/ri', 'react-icons/si', 'react-icons/tw', 'react-icons/vsc'],
+      external: ['react-icons/fa','react-icons/fa6', 'react-icons/ci', 'react-icons/fi', 'react-icons/md', 'react-icons/io', 'react-icons/go', 'react-icons/ti', 'react-icons/ai', 'react-icons/gr', 'react-icons/im', 'react-icons/bi', 'react-icons/bs', 'react-icons/cg', 'react-icons/fc', 'react-icons/hi', 'react-icons/io5', 'react-icons/ri', 'react-icons/si', 'react-icons/tw', 'react-icons/vsc'],
     },
   },
 });
